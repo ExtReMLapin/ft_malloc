78242c6ad02ae01eb0c0978247f74dc80109b375
Pierre FICHEPOIL <pfichepo@e2r10p17.42.fr>
2017-12-14 13:48:33 +0100

    first commit

---
+++ malloc.c
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pfichepo <pfichepo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/14 10:59:14 by pfichepo          #+#    #+#             */
+/*   Updated: 2017/12/14 13:46:26 by pfichepo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "malloc.h"
+#include <stdio.h>
+
+//mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
+
+	
+static unsigned long int page_size(bool big)
+{
+	unsigned int i;
+
+	i = sizeof(t_plage);
+	if (big)
+		i += MED_PAGE_SIZE;
+	else
+		i += TINY_PAGE_SIZE;
+	return (i);
+}
+
+
+static void *ezmmap(unsigned long int size)
+{
+	return mmap(0, size, PROT, MAP, -1, 0);
+}
+
+static void init_page(t_plage *plage, size_t size)
+{
+	plage->data = NULL;
+	plage->next	= NULL;
+	plage->size = size;
+}
+
+
+static void* find_freespace(t_plage *plage, size_t wanted)
+{
+	t_malloc *curmalloc;
+	if (wanted > plage->size)
+		return NULL;
+
+	if (plage->data == NULL)
+		return (&plage->data);
+	curmalloc = plage->data;
+	while (curmalloc->next) // searching for free space between already existings mallocs
+	{
+		if ((curmalloc->end+1 - curmalloc->next ) > wanted) // find the first good place, not scanning the whole plages
+			return (curmalloc->end + 1);
+		curmalloc = curmalloc->next;
+	}	
+
+	if ((curmalloc->end + 1 + wanted) <= plage->max_allowed_alloc) // if nothing between allocs then put it at the end
+		return (curmalloc->end + 1);
+	return (NULL);
+}
+
+static void* find_free_space_plages(t_plage *plage, size_t wanted)
+{
+	bool	found;
+	void	*target;
+	t_plage *plagebrowse;
+
+	found = false;
+	plagebrowse = plage;
+	while (plagebrowse)
+	{
+		target = find_freespace(plagebrowse, wanted);
+		if (target != NULL)
+		{
+			found = true;
+			break;
+		}
+		plagebrowse = plagebrowse->next;
+	}
+	if (!found)
+	{
+		plagebrowse->next = ezmmap(plagebrowse->size);
+		init_page(plagebrowse->next, plagebrowse->size);
+		return (&(plagebrowse->next->data));
+	}
+	else
+		return (target);
+}
+
+
+
+void *init_malloc(void* ptr, size_t size)
+{
+	if (ptr == NULL)
+		return (NULL);
+	t_malloc *mlc;
+	mlc = (t_malloc*)ptr;
+	mlc->end = &mlc->data + size;
+	return (&mlc->data);
+}
+
+
+void *_malloc(size_t size)
+{
+	t_plage	*target;
+	void 	*adr;
+	if (size == 0)
+		return NULL;
+	if (size < MAX_TINY_SIZE)
+	{
+		target = alc_mng.small_plage;
+	}
+	else if (size < MAX_MED_SIZE)
+		target = alc_mng.med_plage;
+	else
+		target = NULL;
+	adr = find_free_space_plages(target, size + sizeof(t_malloc));
+	if (adr != NULL)
+		return (init_malloc(adr, size));
+	return (NULL);	
+}
+
+
+int main(void)
+{
+	void *page = ezmmap(page_size(false) + page_size(true));
+	alc_mng.small_plage = (t_plage*)page;
+	init_page(alc_mng.small_plage, TINY_PAGE_SIZE);
+	alc_mng.med_plage = (t_plage*)(page + page_size(false));
+	init_page(alc_mng.med_plage, MED_PAGE_SIZE);
+
+	printf("%p\n", &alc_mng);
+	printf("%p\n", &alc_mng.small_plage);
+	printf("%p\n", &alc_mng.med_plage);
+	printf("%p\n", alc_mng.med_plage);
+
+	return 0;
+}
+
+
---
+++ malloc.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pfichepo <pfichepo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/14 10:49:33 by pfichepo          #+#    #+#             */
+/*   Updated: 2017/12/14 13:01:09 by pfichepo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MALLOC_H
+# define MALLOC_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/mman.h>
+# include <errno.h>
+# include <stdbool.h>
+
+# define MAX_TINY_SIZE 8
+# define TINY_PAGE_SIZE (getpagesize())
+# define MAX_MED_SIZE 64
+# define MED_PAGE_SIZE (8 * getpagesize())
+# define PROT			PROT_READ | PROT_WRITE
+# define MAP			MAP_ANON | MAP_PRIVATE
+
+typedef struct			s_malloc
+{
+	void 				*next;
+	void 				*end;
+	void 				*data;
+}	t_malloc;
+
+
+typedef struct			s_plage
+{
+	size_t				size;
+	void				*max_allowed_alloc;
+	struct s_plage		*next;
+	t_malloc			*data;
+}	t_plage;
+
+typedef struct			s_plage_mng
+{
+	struct s_plage		*small_plage;
+	struct s_plage		*med_plage;
+}	t_plage_mng;
+
+t_plage_mng				alc_mng;
+
+#endif
