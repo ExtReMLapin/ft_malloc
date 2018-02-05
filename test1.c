8914951059096b388406dd40833de27161cdc718
Pierre FICHEPOIL <pfichepo@e2r9p14.42.fr>
2018-02-05 12:49:39 +0100

    fixed plage error

--- test1.c
+++ test1.c
@@ -6,36 +6,11 @@
 /*   By: pfichepo <pfichepo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/08 14:49:06 by byoung-w          #+#    #+#             */
-/*   Updated: 2018/02/05 12:10:35 by pfichepo         ###   ########.fr       */
+/*   Updated: 2018/02/05 12:40:25 by pfichepo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/malloc.h"
-
-static void fill(char *str, size_t size) // on remplis la string de char de la table ascii, pas que le meme pour detecter le decalage possible
-{
-	size_t i = 0;
-	while (i < size)
-	{
-		str[i] = 33 + i%94;
-		i++;
-	}
-	str[i] = 0;
-}
-
-
-static bool eq(char *str, char* str2) // on check == des 2 strings, s'arrete a la fin de la plus petite string
-{
-	int i = 0;
-
-	while (str[i] && str2[i])
-	{
-		if (str[i] != str2[i])
-			return (false);
-		i++;
-	}
-	return (true);
-}
 
 
 int			main(void)
